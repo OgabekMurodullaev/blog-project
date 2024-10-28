b7def6b82bb96c28ee50a377b750fea3fe3da2af
OgabekMurodullaev <148078268+OgabekMurodullaev@users.noreply.github.com>
2024-10-28 18:37:58 +0500

    edit template

--- posts/models.py
+++ posts/models.py
@@ -47,7 +47,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     comment = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
